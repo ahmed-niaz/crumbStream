125d4a1882ecd6e8fc78bc73df309e9887d9864c
ahmed-niaz <niazahmed.an@gmail.com>
2026-09-07 16:42:42 +0600

    initial step for the auth-service

---
+++ auth-service/eslint.config.ts
@@ -0,0 +1,22 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  {
+    ignores: ["node_modules/", "dist/", "**/pnpm-lock.yaml"],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    rules: {
+      "no-console": "warn",
+    },
+  },
+);
---
+++ auth-service/src/server.ts
@@ -0,0 +1,10 @@
+import app from "./app";
+import { Server } from "http";
+const port = 3000;
+
+async function main() {
+  const server: Server = app.listen(port, () => {
+    console.log("auth server is running ⚡", port);
+  });
+}
+main();
